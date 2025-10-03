091b8416e171a29d383f79f9c13b540b128aba05
popovicfilip9-ux <popovic.filip9@googlemail.com>
2025-10-03 12:41:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RISK-Tool/'
+  base: '/RISK-Tool/',
+  build: {
+    rollupOptions: {
+      input: './main.jsx'
+    }
+  }
 })
